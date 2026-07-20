8480206816a252e485306e13e7286494ca027db6
lenixdev <149199196+LenixDev@users.noreply.github.com>
2026-07-21 00:36:33 +0100

    refactor(users): migrate users table to dashboard routes and introduce dropdown menu for user actions

--- src/app/dashboard/routes/users.tsx
+++
@@ -1,7 +0,0 @@
-import { UsersTable } from "@/components/users"
-
-export const Users = () => (
-  <div className="h-full w-full flex items-center justify-center">
-    <UsersTable />
-  </div>
-)
---
+++ src/app/dashboard/routes/users/index.tsx
@@ -0,0 +1,24 @@
+import { DataTable } from "@/app/dashboard/routes/users/rows"
+import { useColumns } from "@/app/dashboard/routes/users/columns"
+import { useUsers } from "@/hooks/use-users"
+import type { UserAccount } from "@/types"
+import { useTranslation } from "react-i18next"
+import { isRole } from "@/lib"
+
+export const Users = () => {
+	const { t } = useTranslation()
+
+	const data: UserAccount[] = useUsers().map((user) => ({
+		...user,
+		roleLabel: isRole(user.role) ? t(`roles_alias.${user.role}`) : user.role,
+	}))
+	const columns = useColumns()
+
+	return (
+	<div className="h-full w-full flex items-center justify-center">
+		<div className="max-w-4/5 container mx-auto py-10">
+			<DataTable columns={columns} data={data} />
+		</div>
+	</div>
+	)
+}
